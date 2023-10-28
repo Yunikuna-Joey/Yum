1563818b2a0e3cb2b46d2b87020917d757ae6ca8
Joey <37638087+Yunikuna-Joey@users.noreply.github.com>
2023-10-27 23:37:45 -0700

    debugging required

--- static/main.js
+++ static/main.js
@@ -118,6 +118,66 @@ function register() {
     return false;
 } 
 
+// Google Maps Functions located here 
+function initMap() {
+    // creating the instance of a map within the provided html tag / position
+    const map = new google.maps.Map(document.getElementById('map-section'), {
+        // default values of 0, 0 
+        center: {user_lat: 43.400344826187, user_lng: -80.3250596245924},
+        zoom: 12 
+    });
+    // error seems to be in this first portion with center being declared 
+
+
+    // // Using 'geolocation' support as a way to parse user lat and long 
+    // if (navigator.geolocation) {        // I think this is when the user is prompted && presses allow location premissions
+    //     navigator.geolocation.getCurrentPosition(function (position) {
+    //         const user_coordinates = {
+    //             user_lat: position.coords.latitude,
+    //             user_lng: position.coords.longitude
+    //         };
+            
+    //         // DEBUGGING PLEASE LAT AND LONG
+    //         console.log('*************************************');
+    //         console.log('Lat: ', user_coordinates.user_lat);
+    //         console.log('Long: ', user_coordinates.user_lng);
+
+    //         // Allow us to center the map on user location
+    //         map.setCenter(user_coordinates);
+
+    //         // Creating a marker to distinctly see where user is 
+    //         const marker = new google.maps.Marker({
+    //             position: user_coordinates, 
+    //             map: map, 
+    //             title: 'Current Location'
+    //         });
+
+    //     }, function(error) {
+    //         // In the event that user denies access 
+    //         console.error('Geolocation error: ', error)
+    //     });
+    // }
+
+    // else {
+    //     // In the event that geolocation is not supported by the browser
+    //     console.error('Geolocation is not supported by your browser.');
+    // } 
+}
+
+// function initMap() {
+//     const map = new google.maps.Map(document.getElementById('hero'), {
+//         center: { lat: 38.42510222646131, lng: -121.39244868818646}, 
+//         zoom: 15
+//     });
+
+//     const marker = new google.maps.Marker({
+//         position: {lat: 38.42510222646131 , lng: -121.39244868818646}, 
+//         map: map, 
+//         title: 'Boba'
+//     });
+// } 
+
+
 function logout() {
     // debugging here 
     console.log('Javascript logout');
