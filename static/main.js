b9413c066e7fe186c7e4ca02d14f5de789bc1efc
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-01-08 00:32:41 -0800

    [WIP] work on styling
    style the marker content with inline css and add like functionality

--- static/main.js
+++ static/main.js
@@ -912,7 +912,16 @@ function createRestMarker(locationData) {
                         const followingReviews = JSON.parse(followingReviewRequest.responseText);
                         markerContent += '<strong>Following Reviews: </strong> <br>';
                         for (const review of followingReviews) {
-                            markerContent += `<div>${review.content} - ${review.rating} by ${review.author_display_name} (${review.author_username})</div><br>`;
+                            markerContent += `
+                                <div id="status-container">
+                                    <table id="status-table"> 
+                                        <tr>
+                                            ${review.content} - ${review.rating} by ${review.author_display_name} (${review.author_username})
+                                        </tr>
+                                    </table>
+                                </div>
+                                <br>
+                            `;
                         }
                         markerWindow.innerHTML = markerContent;
                         markerWindow.style.display = 'block';
